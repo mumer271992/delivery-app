01c927f095d99d349ba4e1f8da9d7e560c65e576
Muhammad Umer <mumer271992@gmail.com>
2019-11-17 11:38:03 +0500

    Server file added

---
+++ server.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const express = require('express');
+const serveStatic = require('serve-static');
+
+const app = express();
+app.use(serveStatic(`${__dirname}/dist`));
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log(`server started + ${port}`);
